0e74ca045d1f471e2d07ad658d49c59e1ae568e2
Stéphane Bélanger <ste-bel@outlook.com>
2026-06-28 18:43:13 +0000

    Add client-side search

--- engine/src/build.ts
+++ engine/src/build.ts
@@ -81,7 +81,7 @@ function layout(title: string, body: string): string {
 <a href="/chinese-character-atlas/lessons/">Lessons</a> ·
 <a href="/chinese-character-atlas/words/">Words</a> ·
 <a href="/chinese-character-atlas/characters/">Characters</a> ·
-<a href="/chinese-character-atlas/components/">Components</a>
+<a href="/chinese-character-atlas/components/">Components</a> ·\n<a href="/chinese-character-atlas/search.html">Search</a>
 </nav>
 ${body}
 </main>
